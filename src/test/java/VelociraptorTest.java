c73f9e7d2048b09e7153b1fc07de2594e6971576
Juan <juanmataruiz84@gmail.com>
2018-06-05 14:41:34 +0100

    dinosaur test can/cannot hunt

--- src/test/java/VelociraptorTest.java
+++ src/test/java/VelociraptorTest.java
@@ -9,50 +9,52 @@ import static org.junit.Assert.assertEquals;
 
 public class VelociraptorTest {
 
-    Velociraptor velociraptor;
+    Velociraptor velociraptor1, velociraptor2;
     Paddock paddock;
 
     @Before
     public void setup() {
-        velociraptor = new Velociraptor(3, 7, DinosaurType.OMNIVORE);
+        velociraptor1 = new Velociraptor(3, 7, DinosaurType.OMNIVORE);
+        velociraptor2 = new Velociraptor(6, 4, DinosaurType.OMNIVORE);
         paddock = new Paddock("Velociraptor Paddock", PaddockType.PREDATORS);
     }
 
     @Test
     public void dinosaurHasType() {
-        assertEquals(DinosaurType.OMNIVORE, velociraptor.getType());
+        assertEquals(DinosaurType.OMNIVORE, velociraptor1.getType());
     }
 
     @Test
     public void dinosaurHasStomach() {
-        assertEquals(3, velociraptor.getStomach());
+        assertEquals(3, velociraptor1.getStomach());
     }
 
     @Test
     public void dinosaurCanBeFed() {
-        velociraptor.dinosaurIsFed();
-        assertEquals(4, velociraptor.getStomach());
+        velociraptor1.dinosaurIsFed();
+        assertEquals(4, velociraptor1.getStomach());
     }
 
     @Test
     public void dinosaurCanRampage() {
-        assertEquals(7, velociraptor.getRampage());
+        assertEquals(7, velociraptor1.getRampage());
     }
 
     @Test
     public void dinosaurCanHunt(){
-        paddock.addDinosaur(velociraptor);
-        velociraptor.getHunt(paddock);
+        paddock.addDinosaur(velociraptor1);
+        velociraptor1.getHunt(paddock);
         assertEquals(0, paddock.getCount());
-        assertEquals(4, velociraptor.getStomach());
+        assertEquals(4, velociraptor1.getStomach());
     }
 
-//    @Test
-//    public void dinosaurCannotHunt() {
-//        paddock.addDinosaur();
-//        velociraptor.getRampage();
-//        assertEquals(3, velociraptor.getStomach());
-//    }
+    @Test
+    public void dinosaurCannotHunt() {
+        paddock.addDinosaur(velociraptor2);
+        velociraptor2.getHunt(paddock);
+        assertEquals(1,paddock.getCount());
+        assertEquals(6, velociraptor2.getStomach());
+    }
 
 
 
