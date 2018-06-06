ec65e002cba5e43d103542f0e26515fb9f6da6f1
Juan <juanmataruiz84@gmail.com>
2018-06-06 13:44:46 +0100

    ankylosaurus class can rampage and break free

--- src/main/java/Dinosaur/Ankylosaurus.java
+++ src/main/java/Dinosaur/Ankylosaurus.java
@@ -1,11 +1,18 @@
 package Dinosaur;
 
+import Areas.Paddock;
 import Enum.DinosaurType;
 
 public class Ankylosaurus extends Specie {
 
     public Ankylosaurus(int stomach, int rampage, DinosaurType dinosaurType) {
         super(stomach, rampage, dinosaurType);
+    }
+
+    public void hunt(Paddock paddock) {
+        if (getRampage() >= this.rampage ) {
+            paddock.removeDinosaur();
+        }
     }
 
 
--- src/test/java/AnkylosaurusTest.java
+++ src/test/java/AnkylosaurusTest.java
@@ -1,17 +1,23 @@
+import Areas.Paddock;
 import Dinosaur.Ankylosaurus;
 import Enum.DinosaurType;
+import Enum.PaddockType;
+import ThemePark.Park;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import static org.junit.Assert.assertEquals;
 
 public class AnkylosaurusTest {
 
     Ankylosaurus ankylosaurus;
+    Paddock paddock;
 
     @Before
     public void setup() {
         ankylosaurus = new Ankylosaurus(1, 5, DinosaurType.HERBIVORE);
+        paddock = new Paddock("Galliminus Valley", PaddockType.FREE_FROM_PREDATORS);
     }
 
     @Test
@@ -29,4 +35,34 @@ public class AnkylosaurusTest {
         ankylosaurus.dinosaurIsFed();
         assertEquals(2, ankylosaurus.getStomach());
     }
+
+    @Test
+    public void dinosaurCanRampage() {
+        Ankylosaurus spyAnkylosaurus = Mockito.spy(ankylosaurus);
+        Mockito.when(spyAnkylosaurus.getRampage()).thenReturn(5);
+
+        assertEquals(5, spyAnkylosaurus.getRampage());
+    }
+
+    @Test
+    public void dinosaurCanHunt(){
+        Ankylosaurus spyAnkylosaurus = Mockito.spy(ankylosaurus);
+        Mockito.when(spyAnkylosaurus.getRampage()).thenReturn(7);
+
+        paddock.addDinosaur(spyAnkylosaurus);
+        spyAnkylosaurus.hunt(paddock);
+        assertEquals(0, paddock.getCount());
+    }
+
+    @Test
+    public void dinosaurCannotHunt() {
+        Ankylosaurus spyAnkylosaurus = Mockito.spy(ankylosaurus);
+        Mockito.when(spyAnkylosaurus.getRampage()).thenReturn(4);
+
+        paddock.addDinosaur(spyAnkylosaurus);
+        spyAnkylosaurus.hunt(paddock);
+        assertEquals(1, paddock.getCount());
+    }
+
+
 }
